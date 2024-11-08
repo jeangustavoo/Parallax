57e71b128b049f869c765a517c1b6e6fdbd6ef2d
jean <jeangustavofut@gmail.com>
2024-11-08 10:51:01 -0300

    08/11

--- Parallax/MainPage.xaml.cs
+++ Parallax/MainPage.xaml.cs
@@ -2,43 +2,98 @@
 
 public partial class MainPage : ContentPage
 {
-	bool EstaMorto = false;
-	bool EstaPulando = false;
-	const int TempoEntreFrames = 25;
+	bool estamorto = false;
+	bool estapulando = false;
+	const int tempoentreframes = 25;
+	int velocidade = 0;
 	int velocidade1 = 0;
 	int velocidade2 = 0;
-	int velocidade = 0;
-	int LarguraJanela = 0;
-	int AlturaJanela = 0;
+	int velocidade3 = 0;
+	int larguraJanela = 0;
+	int alturaJanela = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-    protected override void OnSizeAllocated(double width, double height)
-    {
-        base.OnSizeAllocated(width, height);
-		CorrigeTamanhoCenario(width, height);
-		CalculaVelocidade(width);
-    }
-	void CalculaVelocidade(double width)
-	{
-		velocidade1 = (int)(width * 0.001);
-		velocidade2 = (int)(width * 0.004);
-		velocidade = (int)(width * 0.01);
-	}
-	void CorrigeTamanhoCenario(double width, double height)
-	{
-		foreach (var ceu in HsLayer1.Children)
-		(ceu as Image).WidthRequest = width;
-		foreach (var predio in HsLayer2.Children)
-		(predio as Image).WidthRequest = width;
-		foreach (var chao in HsLayer4Chao.Children)
-		(chao as Image).WidthRequest = width;
 
-		HsLayer1.WidthRequest = width * 1.5;
-		HsLayer2.WidthRequest = width  * 1.5;
-		HsLayer4Chao.WidthRequest = width  * 1.5;
+	async Task Desenhar()
+	{
+		while (!estamorto)
+		{
+			GerenciaImagens();
+			await Task.Delay(tempoentreframes);
+		}
 	}
+
+	void GerenciaImagens(HorizontalStackLayout HSL)
+	{
+		var view = (HSL.Children.First() as Image);
+		if (view.WidthRequest + HSL.TranslationX <0)
+		{
+			HSL.Children.Remove(view);
+			HSL.Children.Add(view);
+			HSL.TranslationX=view.TranslationX;
+		}
+	}
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		CalculaVelocidade(w);
+		CorrigeTamanho(w, h);
+	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenhar();
+	}
+	void CalculaVelocidade(double w)
+	{
+		velocidade = (int)(w * 0.01);
+		velocidade1 = (int)(w * 0.001);
+		velocidade2 = (int)(w * 0.004);
+		velocidade3 = (int)(w * 0.008);
+	}
+
+	void CorrigeTamanho(double w, double h)
+	{
+		foreach (var a in HsLayer1.Children)
+			(a as Image).WidthRequest = w;
+		foreach (var a in HsLayer2.Children)
+			(a as Image).WidthRequest = w;
+		foreach (var a in HsLayer4Chao.Children)
+			(a as Image).WidthRequest = w;
+
+		HsLayer1.WidthRequest = w;
+		HsLayer2.WidthRequest = w;
+		HsLayer4Chao.WidthRequest = w;
+	}
+
+	void MoveCenario()
+	{
+		HsLayer1.TranslationX-=velocidade1;
+		HsLayer2.TranslationX-=velocidade2;
+        HsLayer4Chao.TranslationX-=velocidade;
+	}
+
+	void GerenciaImagens()
+	{
+		MoveCenario();
+		GerenciaImagens(HsLayer1);
+		GerenciaImagens(HsLayer2);
+		GerenciaImagens(HsLayer4Chao);
+	}
+
+	async Task Desenha()
+	{
+		while (!estamorto)
+		{
+			GerenciaImagens();
+			await Task.Delay(tempoentreframes);
+		}
+	}
+	
+	
+
 }
-
